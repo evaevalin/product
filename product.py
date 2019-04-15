76dc1c292b3972b86db75ad72aeec44e5d6c7c3b
luli <evalin19920321@gmail.com>
2019-04-15 10:06:55 +0800

    add continue to remove the 商品價格欄位

--- product.py
+++ product.py
@@ -2,14 +2,13 @@ products = []
 #讀取檔案
 with open('product.csv','r', encoding = 'utf-8') as f:
     for line in f:
+        if '商品,價格'in line:
+            continue # 跳過下面 直接到下一回
         name, price = line.strip().split(',')
         products.append([name, price])
 print(products)
 
-
-
-
-
+#讓使用者輸入
 while True:
     name = input('請輸入商品名稱: ')
     if name == 'q':   # q=quit
@@ -23,9 +22,8 @@ while True:
 
     products.append([name,  price])
 
-
+#印出所有購買商品紀錄
 print(products)
-
 for p in products:
     print(p[0], '價格是', p[1], '元')
 
@@ -34,8 +32,8 @@ for p in products:
 #     for p in products:
 #         f.write(p[0]+','+ str(p[1]) + '\n')
 
-#csv檔案 可以用excel開
+#寫入檔案， 可以用excel開
 with open('product.csv', 'w', encoding = ' utf-8') as f:
     f.write('商品,價格\n')
-    for p in products:
+    for p in products:     #每個商品
         f.write(p[0]+','+ str(p[1]) + '\n')
